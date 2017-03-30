74240a773924933460e429de72baa7b6ec982990
andrewdc92 <andrew.cordivari@gmail.com>
2017-03-30 15:42:24 -0700

    document json data renders on port 3001. nice

--- freelance_documents/db/seeds.rb
+++ freelance_documents/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+8.times do |d|
+  FreelanceDocument.create!(
+title: "Document #{d}",
+description: "Today is thursday, it's an excellent day.
+tons of work accomplished. can't believe tomorrow is Friday already. SAD!",
+file_url: "https://docs.google.com/document/d/1CYIPNjGh_AzSN_koyFow2eUWhz9wHEPBEYAx_GhnoPM/edit?usp=sharing",
+image_url: "http://static.boredpanda.com/blog/wp-content/uploads/2015/11/maine-coon-cats-2__605.jpg"
+)
+end
